b82576a75cd47bb6814fb3b57253d708983cc0c9
bkturley <kyle@kturley.com>
2013-03-12 22:00:42 -0500

    will this work

---
+++ weatherApp/UIView+_additions.h
@@ -0,0 +1,13 @@
+//
+//  UIView+_additions.h
+//  weatherApp
+//
+//  Created by BKTurley on 3/12/13.
+//  Copyright (c) 2013 BKTurley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (_additions)
+
+@end
---
+++ weatherApp/UIView+_additions.m
@@ -0,0 +1,15 @@
+//
+//  UIView+_additions.m
+//  weatherApp
+//
+//  Created by BKTurley on 3/12/13.
+//  Copyright (c) 2013 BKTurley. All rights reserved.
+//
+
+#import "UIView+_additions.h"
+
+@implementation UIView (_additions)
+
+
+
+@end
